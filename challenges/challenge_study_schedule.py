9b56989f5b813856a35f6a967b2573636ad1d8a6
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-25 15:20:13 -0300

    req 01 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    count = 0
+    for period in permanence_period:
+        if not all([isinstance(item, int) for item in period]):
+            return None
+        if period[0] <= target_time <= period[1]:
+            count += 1
+    return count
