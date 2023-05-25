b920c84eeedee91b60f00026de122751fbaab5fc
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-25 15:53:53 -0300

    test: req 02 done

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("olá", "oi")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+    assert encrypt_message("bola", 5) == "alob"
+    assert encrypt_message("padaria", 3) == "dap_aira"
+    assert encrypt_message("urso", 2) == "os_ru"
