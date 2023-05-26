ee393fac123495dcafe40b3abcc74356a268694c
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-26 16:29:13 -0300

    feat: req 05 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums:
+        return False
+    nums.sort()
+    for index in range(len(nums)):
+        if (
+            index + 1 >= len(nums)
+            or not isinstance(nums[index + 1], int)
+            or not isinstance(nums[index], int)
+            or nums[index] < 0
+           ):
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+    return False
+
